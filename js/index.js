f83306f79413af31479acec38f699e01f454301c
CesarMeier <108493050+CesarMeier@users.noreply.github.com>
2022-11-04 12:48:38 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".navestilo")
+
+navToggle.addEventListener("click", () => {navMenu.classList.toggle("navestilo_visible");});
